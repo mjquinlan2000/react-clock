873215886ee98caa8847412926b79a48a837468e
Mike Quinlan <mquinlan@enbala.com>
2016-10-28 11:45:52 -0600

    master: Making sure this thing runs faster

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   context: path.join(__dirname, 'app'),
@@ -25,5 +26,10 @@ module.exports = {
     alias: {
       app: path.join(__dirname, 'app')
     }
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
+    })
+  ]
 };
